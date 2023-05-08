a9aa587cd35e4c3c4e4f85454873394bd81189e2
Luis Beltran <luisignaciobeltran5@gmail.com>
2023-05-07 17:46:18 -0700

    File Reader, reads a txt file counts unique words and excludes repeated words. The program writes a String to a file "text_unique_words.txt", which reads the unique words and gives you the number of unique words.

--- src/main/java/com/solvd/laba/FileReader.java
+++ src/main/java/com/solvd/laba/FileReader.java
@@ -1,9 +1,40 @@
 package com.solvd.laba;
 
+// importing necessary libraries
+import java.io.File;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class FileReader {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        try {
+            // To read the text from the input file
+            String textInput = FileUtils.readFileToString(new File("text_to_read.txt"), "UTF-8");
+
+            // To split the text into words
+            String[] words = StringUtils.split(textInput);
+
+            // To create a HashSet to store the unique words
+            Set<String> uniqueWords = new HashSet<>();
+
+            // To Iterate over each word and add to the HashSet
+            for (String word : words) {
+                uniqueWords.add(word);
+            }
+
+            // To get the number of unique words
+            int uniqueWordCount = uniqueWords.size();
+
+            // To write the result into a new file
+            FileUtils.writeStringToFile(new File("text_unique_words.txt"), "Unique word count: " + uniqueWordCount, "UTF-8");
+
+            System.out.println("Unique word count: " + uniqueWordCount);
+        } catch (IOException e) {
+            System.err.println("Error reading or writing file: " + e.getMessage());
+        }
     }
 }
